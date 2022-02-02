ac084602a26b8eebe0c62f4323cad2678f2a189d
Sriram Chinthoti <82876727+sriram503@users.noreply.github.com>
2022-02-03 02:50:03 +0530

    Add files via upload

--- check.cpp
+++ check.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 void initalizeBoard(char A[3][3])
 {
     for(int i=0;i<3;i++)
@@ -63,23 +64,106 @@ int check(char A[3][3] , int moves)
 {
     for(int i = 0 ; i < 3 ; ++i)
     {
-        if(A[i][0] == 'O' && A[i][1] == 'O' && A[i][2] == 'O') return 2;
+        if(A[i][0] == 'O' && A[i][1] == 'O' && A[i][2] == 'O') return -1;
         if(A[i][0] == 'X' && A[i][1] == 'X' && A[i][2] == 'X') return 1;
     }
 
     for(int i = 0 ; i < 3 ; ++i)
     {
-        if(A[0][i] == 'O' && A[1][i] == 'O' && A[2][i] == 'O') return 2;
+        if(A[0][i] == 'O' && A[1][i] == 'O' && A[2][i] == 'O') return -1;
         if(A[0][i] == 'X' && A[1][i] == 'X' && A[2][i] == 'X') return 1;
     }
 
-    if(A[0][0] == 'O' && A[1][1] == 'O' && A[2][2] == 'O') return 2;
+    if(A[0][0] == 'O' && A[1][1] == 'O' && A[2][2] == 'O') return -1;
     if(A[0][0] == 'X' && A[1][1] == 'X' && A[2][2] == 'X') return 1;
-    if(A[0][2] == 'O' && A[1][1] == 'O' && A[2][0] == 'O') return 2;
+    if(A[0][2] == 'O' && A[1][1] == 'O' && A[2][0] == 'O') return -1;
     if(A[0][2] == 'X' && A[1][1] == 'X' && A[2][0] == 'X') return 1;
 
-    if(moves == 9) return 3;
+    if(moves == 9) return 0;
 
-    return 4;
+    return 3;
 
+}
+
+int minimax(char A[3][3] , int valid[3][3] , bool flag , int moves)
+{
+    int result = check(A , moves);
+    if(result == 1) return 1;
+    if(result == -1) return -1;
+    if(result == 0 || result == 3) return 0;
+
+    if(flag == false)
+    {
+        int score;
+        int bestscore = 10000000; 
+        for(int i = 0 ; i < 3 ; ++i)
+        {
+            for(int j = 0 ; j < 3; ++j)
+            {
+                if(valid[i][j] == 0)
+                {
+                    A[i][j] = 'O';
+                    valid[i][j] = 1;
+                    score = minimax(A , valid , true , moves+1);
+                    A[i][j]= ' ';
+                    valid[i][j] = 0;
+                    if(bestscore > score)
+                    {
+                        bestscore = score;
+                    }
+                }
+            }
+        }
+        return bestscore;
+    }
+    else
+    {
+        int score;
+        int bestscore = -10000000; 
+        for(int i = 0 ; i < 3 ; ++i)
+        {
+            for(int j = 0 ; j < 3; ++j)
+            {
+                if(valid[i][j] == 0)
+                {
+                    A[i][j] = 'X';
+                    valid[i][j] = 1;
+                    score = minimax(A , valid , false , moves+1);
+                    A[i][j]= ' ';
+                    valid[i][j] = 0;
+                    if(bestscore < score)
+                    {
+                        bestscore = score;
+                    }
+                }
+            }
+        }
+        return bestscore;
+    }
+}
+
+void Bestmove(int *a , int *b , char A[3][3] , int valid[3][3] , int moves)
+{
+    int score = 0;
+    int bestscore = -10000000;
+    for(int i = 0 ; i < 3 ; ++i)
+    {
+        for(int j = 0 ; j < 3 ; ++j)
+        {
+            if(valid[i][j] == 0)
+            {
+                A[i][j] = 'X';
+                valid[i][j] = 1;
+                score = minimax(A , valid , false , moves+1);
+                A[i][j]= ' ';
+                valid[i][j] = 0;
+                if(bestscore < score)
+                {
+                    *a = i;
+                    *b = j;
+                    bestscore = score;
+                }
+            }
+        }
+    }
 }
--- main.cpp
+++ main.cpp
@@ -4,19 +4,82 @@ using namespace std;
 
 int main()
 {
+    srand(time(NULL));
     cout<<"------ TIC TAC TOE ------"<<endl;
     cout<<"\nPress 1 start game (player vs player)\n";
+    cout<<"\nPress 2 start game with computer\n";
     int p;
     cin>>p;
-    if(p != 1) exit(0);
-    else
+    if(p == 2)
     {
         char A[3][3];
-
         char turn='X';
         int moves = 0;
         initalizeBoard(A);
-
+        DisplayBoard(A);
+        int valid[3][3] = {0};
+        while(1)
+        {
+            if(turn == 'X')
+            {
+                int a , b;
+                if(moves == 0)
+                {
+                    a = rand()%3;
+                    b = rand()%3;
+                }
+                else
+                {
+                    Bestmove(&a , &b , A , valid , moves);
+                }
+                A[a][b] = 'X';
+                valid[a][b] = 1;
+                turn = 'O';
+                moves++;
+            }
+            else
+            {
+                cout<<"Enter your move"<<endl;
+                
+                int a,b;
+                cin>>a>>b;
+                while(valid[a][b] !=0)
+                {
+                    cout<<"Invalid Move!! Enter your move again"<<endl;
+                    cin>>a>>b;
+                }
+                A[a][b] = 'O';
+                valid[a][b] = 1;
+                turn = 'X';
+                moves++;
+            } 
+            system("clear");
+            DisplayBoard(A);
+            int result = check(A , moves);
+            if(result == 1)
+            {
+                cout<<"X wins\n";
+                break;
+            }
+            else if(result == -1)
+            {
+                cout<<"O wins\n";
+                break;
+            }
+            else if(result == 0)
+            {
+                cout<<"Match Drawn\n";
+                break;
+            }
+        }
+    }
+    else
+    {
+        char A[3][3];
+        //char B[3][3];
+        char turn='X';
+        int moves = 0;
+        initalizeBoard(A);
         DisplayBoard(A);
         int valid[3][3] = {0};
         while(1)
@@ -61,12 +124,12 @@ int main()
                 cout<<"X wins\n";
                 break;
             }
-            else if(result == 2)
+            else if(result == -1)
             {
                 cout<<"O wins\n";
                 break;
             }
-            else if(result == 3)
+            else if(result == 0)
             {
                 cout<<"Match Drawn\n";
                 break;
